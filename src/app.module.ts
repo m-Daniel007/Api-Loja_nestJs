fd651c5b7ca7e26c100aa22058fde1d63874ad0d
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-07-06 14:36:59 -0300

    feat: :sparkles: criando arquivo de conexao com postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProdutoModule } from './produto/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { PostgresConfigService } from './config/postgres.config.service';
 import { UsuarioModule } from './usuarios/usuario.module';
 
-
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [
+    UsuarioModule,
+    ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
