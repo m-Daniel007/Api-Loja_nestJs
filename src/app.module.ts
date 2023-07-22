cf4e2fdb12a129fa4f9316f599bdb4c37c77d514
Marcos <marcosdanieldossantos@yahoo.com.br>
2023-07-22 11:52:08 -0300

    feat:

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { PostgresConfigService } from './config/postgres.config.service';
 import { PedidoModule } from './pedido/pedido.module';
 import { MorganMiddleware } from '@nest-middlewares/morgan';
-import { FiltroDeExcecaoHttp } from './filtros/filtros-excessao-http';
+import { FiltroDeExcecaoGlobal } from './filtros/filtros-excessao-global';
 import { APP_FILTER } from '@nestjs/core';
 
 @Module({
@@ -25,7 +25,7 @@ import { APP_FILTER } from '@nestjs/core';
   providers: [
     {
       provide: APP_FILTER,
-      useClass: FiltroDeExcecaoHttp,
+      useClass: FiltroDeExcecaoGlobal,
     },
   ],
 })
